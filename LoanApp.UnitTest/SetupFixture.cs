2b2803532a0c17096091c56435e06b065c2066c7
Sundeep Monagoud <sundeepm@tixtechnology.com>
2019-10-02 00:42:45 +1000

    Added unit test case for controller project

---
+++ LoanApp.UnitTest/SetupFixture.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using LoanApp.BAL;
+using LoanApp.DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoanApp.UnitTest
+{
+    [TestClass]
+    public static class SetupFixture
+    {
+        internal static string ConnectionString;
+
+        internal static IMapper Mapper;
+      
+
+        internal static ICustomerLoanServiceBAL CustomerLoanServiceBAL;
+        internal static ICustomerLoanServiceDAL CustomerLoanServiceDAL;
+        internal static ILoanServiceDAL LoanServiceDAL;
+    }
+}
